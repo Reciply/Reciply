a75cb5bf6aeba890d253e6d139aaafb5d0b608a6
Arvy <arvy.au@gmail.com>
2020-01-14 22:17:51 +1100

    Setting up frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import { hot } from 'react-hot-loader/root';
+// import './App.css';
+
+function App(){
+  return(
+    <div>
+      This is the App
+    </div>
+  )
+}
+
+export default hot(App);
