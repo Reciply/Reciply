ae2a17a32ec86db7baffc5a482c68be141ec429e
Arvy <arvy.au@gmail.com>
2020-01-15 11:46:43 +1100

    Setting up router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,25 @@
 
 import React from "react";
 import { hot } from 'react-hot-loader/root';
+import { Route, Switch } from 'react-router-dom'
 // import './App.css';
 
 function App(){
   return(
     <div>
-      This is the App
+      <Switch>
+        <Route
+          path="/"
+          render={() => <div>HomePage</div>}
+        />
+        <Route
+          path="/products"
+          render={() => <div>ProductsPage</div>}
+        />
+        <Route path="/">
+          test
+        </Route>
+      </Switch>
     </div>
   )
 }
