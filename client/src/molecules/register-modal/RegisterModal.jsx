44f6e85711a8e6a2c5cf1cfd3d5e56fbe74e4f0d
Arvy <arvy.au@gmail.com>
2020-01-20 00:39:13 +1100

    More css for register Modal

--- client/src/molecules/register-modal/RegisterModal.jsx
+++ client/src/molecules/register-modal/RegisterModal.jsx
@@ -3,15 +3,44 @@ import React, { Component } from 'react'
 import TextField from '../../elements/textfield'
 import Button from '../../elements/button'
 
+import { checkPostCode, register } from '../../api/AuthenticationHelper'
+
 import styles from './RegisterModal.css'
 
 class RegisterModal extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      postCode: '',
+      showRegister: false,
+    }
+  }
+
+  handleChange = (name, value) => {
+    this.setState({
+      [name]: value
+    })
+  }
+
+
   render(){
+    const { postCode } = this.state
     return(
       <div className={styles.modal}>
-        <p>Groceries Delivered to your door</p>
-        <TextField placeholder="Postcode" />
-        <Button>Continue</Button> 
+        <h4>Groceries Delivered to your door</h4>
+        <TextField 
+          name="postCode"
+          className={styles.postCode}
+          placeholder="Postcode"
+          value={postCode}
+          onChange={value => this.handleChange('postCode', value)}
+        />
+        <Button 
+          className={styles.cntnBtn}
+          onClick={this.checkPostCode}
+        >
+          Continue
+        </Button> 
         <p>Already have an account?</p> 
       </div>
     )
