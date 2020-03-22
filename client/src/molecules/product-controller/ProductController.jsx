bb241d5db489e80223afe2e24fe791fcf5044e3a
Arvy <arvy.au@gmail.com>
2020-03-22 22:07:50 +1100

    Join the search bar and category select

--- client/src/molecules/product-controller/ProductController.jsx
+++ client/src/molecules/product-controller/ProductController.jsx
@@ -14,7 +14,8 @@ import {
 
 import TextField from '../../elements/textfield'
 import Button from '../../elements/button'
-import Select from 'react-select'
+import Select, { components } from 'react-select'
+import Creatable from 'react-select/creatable'
 
 import styles from './ProductController.css'
 
@@ -30,89 +31,39 @@ class ProductController extends Component{
 
   componentDidMount(){
     const {
-      categoryOptions,
-      categorySelect
-    } = this.state
-    const {
       getCategoriesConnect,
-      categories
     } = this.props
-
     getCategoriesConnect()
-    // let promise = new Promise(() => getCategoriesConnect())
-    // promise.then((res) => {
-    //   console.log(res)
-    //   console.log(categories)
-    //   const parseCategories = []
-    //   console.log(categories)
-    //   categories.map(category => {
-    //     parseCategories.push({
-    //       label: category.Name,
-    //       value: category.NodeId
-    //     })
-    //   })
-  
-    //   console.log(parseCategories)
-  
-    //   this.setState({
-    //     categoryOptions: [ 
-    //         ...parseCategories
-    //     ]
-    //   })
-    // })
-    // .catch(res => {
-    //   console.log(res)
-    // })
   }
 
-  handleSelectCategory = (value) => {
+
+  handleSearch = (value) => {
+
     const {
+      searchProductsConnect,
       getProductsConnect,
       categories
     } = this.props
-
-    this.setState({
-      search: '',
-      categorySelect: value
-    })
-
-    const selectedCategory = categories.findIndex(c => c.NodeId === value.value)
-
-    console.log(selectedCategory)
-
-    getProductsConnect({
-      categoryId : categories[selectedCategory].NodeId,
-      pageNumber : 1,
-      pageSize : 36, 
-      UrlFriendlyName : categories[selectedCategory].UrlFriendlyName,
-      categoryName : categories[selectedCategory].Name
-    })
-  }
-
-  handleTextChange = (name, value) => {
-    this.setState({
-      [name]: value 
-    })    
-  }
-
-  handleSearch = () => {
-    const {
-      search
-    } = this.state
-    const {
-      searchProductsConnect
-    } = this.props
-
-    this.setState({
-      categorySelect: null
-    })
+    if(value){
+      const selectedCategory = categories.findIndex(c => c.NodeId === value.value)
     
-    if (search){
-      searchProductsConnect({
-        search: search,
-        pageNumber: 1,
-        pageSize: 24,
-      })
+      console.log(selectedCategory)
+  
+      if(selectedCategory >= 0){
+        getProductsConnect({
+          categoryId : categories[selectedCategory].NodeId,
+          pageNumber : 1,
+          pageSize : 36, 
+          UrlFriendlyName : categories[selectedCategory].UrlFriendlyName,
+          categoryName : categories[selectedCategory].Name
+        })
+      }else{
+        searchProductsConnect({
+          search: value.value,
+          pageNumber: 1,
+          pageSize: 24,
+        })
+      }
     }
   }
 
@@ -134,25 +85,28 @@ class ProductController extends Component{
       })
     })
 
+    const DropdownIndicator = props => {
+      return (
+        components.DropdownIndicator && (
+          <components.DropdownIndicator {...props}>
+            <Search/>
+          </components.DropdownIndicator>
+        )
+      );
+    };
+    
+
     return(
       <div className={styles.productController}>
-          <div className={styles.searchBar}>
-            <TextField
-              name="search"
-              className={styles.searchTxt}
-              placeholder="Search ..."
-              value={search}
-              onChange={value => this.handleTextChange('search', value)}
-            />
-          </div>
-          <Button className={styles.searchBtn} onClick={this.handleSearch}>
-            <Search className={styles.searchIcon} />
-          </Button>
-          <Select
+          <Creatable
+            components={{ DropdownIndicator }}
+            placeholder="Select a category or Type to search"
             className={styles.selectCat}
             options={categoryOptions}
-            onChange={value => this.handleSelectCategory(value)}
-          />
+            onChange={value => this.handleSearch(value)}
+            isClearable={true}
+            formatCreateLabel={userInput => `Search for ${userInput}`}
+          />  
       </div>
     )
   }
