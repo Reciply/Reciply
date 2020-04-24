85114b4314c27c3c9b038a850768187330e55aea
Arvy <arvy.au@gmail.com>
2020-04-24 10:26:23 +1000

    Test if i can commit to new repo

--- client/src/elements/button/Button.jsx
+++ client/src/elements/button/Button.jsx
@@ -6,13 +6,13 @@
  * @author Arvy Salazar
  */
 /* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-import styles from './Button.css';
-//TODO: Some of this code can be shortened but is not because of future proofing
-const Button = (props) =>{
-  //The code is the way that it is for future proofing. 
+import styles from './Button.css'
+// TODO: Some of this code can be shortened but is not because of future proofing
+const Button = (props) => {
+  // The code is the way that it is for future proofing.
   const {
     type = 'button',
     isDisabled = false,
@@ -24,8 +24,8 @@ const Button = (props) =>{
 
   const classname = `${styles[appearance]} ${className || ''}`
 
-  return(
-    <button className={classname} onClick={onClick} type={type} disabled={isDisabled} >
+  return (
+    <button className={classname} onClick={onClick} type={type} disabled={isDisabled}>
       {children}
     </button>
   )
@@ -37,7 +37,7 @@ Button.propTypes = {
   className: PropTypes.string,
   isDisabled: PropTypes.bool,
   appearance: PropTypes.string,
-  onClick: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired
 }
 
 export default Button
