50db948d8fb629c6817e7f00d38b336b22845b08
Leyton <hanlei9876@gmail.com>
2020-07-02 04:20:35 +1000

    .gitignore is now working

--- backend/app/controllers/test_1.js
+++
@@ -1,7 +0,0 @@
-const orderSaving = require('./orderSaving');
-
-const myJSON = '{"email": "A@gmail.com", "address": "200 Ray Street, Burwood NSW 2122", "items": [{"name": "lettuce", "price": 9.95, "amount": 2}, {"name": "bread", "price": 3.55, "amount": 1}], "instructions": "I need have it by today, please."}';
-// const obj = JSON.parse(myJSON);
-// console.log(obj);
-
-orderSaving.saveOrder(myJSON);
